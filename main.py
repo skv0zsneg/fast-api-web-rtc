b36c92ce0d8fc7963e72ff44585b74c826b59057
skv0zsneg <you@example.com>
2024-03-25 03:02:52 +0300

    Add TS for front

--- main.py
+++ main.py
@@ -1,106 +1,158 @@
 import json
-import platform
+from enum import Enum
+from typing import Any
+import logging
 
-from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaPlayer, MediaRelay
-from aiortc.rtcrtpsender import RTCRtpSender
-from fastapi import FastAPI
-from fastapi.responses import Response
-from pydantic import BaseModel
 import uvicorn
-
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse, Response
+from pydantic import BaseModel
 
 app = FastAPI()  # fast api app
-pcs = set()  # peer connections
-relay = None
-webcam = None
 
 
-class Offer(BaseModel):
-    sdp: str
-    type: str
+# models, enums etc ###########################################################
 
 
-def create_local_tracks():
-    global relay, webcam
+class MessageSignal(Enum):
+    USER_CONNECTED = "USER_CONNECTED"
+    USER_DISCONNECTED = "USER_DISCONNECTED"
+    NEW_ICE_CANDIDATE = "NEW_ICE_CANDIDATE"
+    VIDEO_OFFER = "VIDEO_OFFER"
+    VIDEO_ANSWER = "VIDEO_ANSWER"
+    HANG_UP = "HANG_UP"
 
-    options = {"framerate": "30", "video_size": "640x480"}
-    if relay is None:
-        if platform.system() == "Darwin":
-            webcam = MediaPlayer(
-                "default:none",
-                format="avfoundation",
-                options=options
-            )
-        elif platform.system() == "Windows":
-            webcam = MediaPlayer(
-                "video=Integrated Camera",
-                format="dshow",
-                options=options
-            )
-        else:
-            webcam = MediaPlayer(
-                "/dev/video0",
-                format="v4l2",
-                options=options
-            )
-        relay = MediaRelay()
-    if webcam:
-        return None, relay.subscribe(webcam.video)
-    raise ValueError("Webcam and Relay is None!")
+
+class Message(BaseModel):
+    signal: MessageSignal
+    content: Any
+
+
+# user storage ################################################################
+
+
+class User(BaseModel):
+    id: str
+    name: str
+
+
+class UserStorage:
+    def __init__(self) -> None:
+        self.users: dict[str, User] = {}
+
+    def create(self, id: str) -> User:
+        created_user = User(
+            id=id,
+            name=f"Пользователь-{len(self.users.values()) + 1}"
+        )
+        self.users[id] = created_user
+        return created_user
+
+    def get(self, id: str | None = None) -> User | list[User]:
+        if id:
+            return self.users[id]
+        return self.users.values()
+
+    def delete(self, id: str) -> None:
+        del self.users[id]
+
+
+user_storage = UserStorage()
+
+
+# con manager #################################################################
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: dict[str, WebSocket] = {}
+
+    async def connect(self, websocket: WebSocket, id: str):
+        await websocket.accept()
+        self.active_connections.update({id: websocket})
+
+    def disconnect(self, id: str):
+        del self.active_connections[id]
+
+    async def send_personal_message(self, message: str, id: str):
+        await self.active_connections[id].send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections.values():
+            await connection.send_text(message)
+
+
+ws_connection_manager = ConnectionManager()
+
+
+# routes ######################################################################
 
 
 @app.get("/")
 async def root():
-    with open("./static/index.html", 'r') as rf:
+    with open("./static/index.html", "r") as rf:
         index_html = rf.read()
-    return Response(
-        content=index_html,
-        media_type="text/html"
-    )
+    return HTMLResponse(index_html)
 
 
 @app.get("/client.js")
 async def client_js():
-    with open("./static/client.js", 'r') as rf:
+    with open("./static/client.js", "r") as rf:
         client_js = rf.read()
-    return Response(
-        content=client_js,
-        media_type="application/javascript"
+    return Response(content=client_js, media_type="application/javascript")
+
+
+# web sockets #################################################################
+
+
+@app.websocket("/ws/{user_uuid}")
+async def websocket_chat(websocket: WebSocket, user_uuid: str):
+    logging.info(f"User connected {user_uuid}")
+    created_user = user_storage.create(user_uuid)
+    await ws_connection_manager.connect(websocket, user_uuid)
+    message = Message(
+        signal=MessageSignal.USER_CONNECTED,
+        content=[user for user in user_storage.get()]
     )
+    await ws_connection_manager.broadcast(message.model_dump_json())
+
+    try:
+        while True:
+            data = await websocket.receive_json()
+            print("GET: ", data)
+
+            if data["signal"] in (
+                MessageSignal.VIDEO_OFFER.value,
+                MessageSignal.NEW_ICE_CANDIDATE.value,
+                MessageSignal.HANG_UP.value,
+                MessageSignal.VIDEO_ANSWER.value,
+            ):
+                await ws_connection_manager.send_personal_message(
+                    json.dumps(data),
+                    data["content"]["userId"]
+                )
+            else:
+                await ws_connection_manager.broadcast(
+                    json.dumps(data)
+                )
+
+    except WebSocketDisconnect:
+        user_storage.delete(user_uuid)
+        ws_connection_manager.disconnect(user_uuid)
+        message = Message(
+            signal=MessageSignal.USER_DISCONNECTED,
+            content=created_user
+        )
+        await ws_connection_manager.broadcast(message.model_dump_json())
 
 
-@app.post("/offer")
-async def offer(offer: Offer):
-    offer_conf = RTCSessionDescription(sdp=offer.sdp, type=offer.type)
-    pc = RTCPeerConnection()
+# enter point #################################################################
 
-    pcs.add(pc)
-
-    @pc.on("connectionstatechange")
-    async def on_connectionstatechange():
-        print("Connection state is %s" % pc.connectionState)
-        if pc.connectionState == "failed":
-            await pc.close()
-            pcs.discard(pc)
-
-    await pc.setRemoteDescription(offer_conf)
-
-    answer = await pc.createAnswer()
-    if answer:
-        await pc.setLocalDescription(answer)
-
-    return Response(
-        content=Offer(
-            sdp=pc.localDescription.sdp,
-            type=pc.localDescription.type
-        ).model_dump_json(),
-        media_type="application/json"
-    )
 
 if __name__ == "__main__":
-    running_app = uvicorn.run(
-        app=app,
+    uvicorn.run(
+        app="main:app",
         host="localhost",
         port=8000,
+        reload=True,
     )
--- static/client.ts
+++ static/client.ts
@@ -1,92 +1,151 @@
 // utils *********************************************************************/
-function generateUUIDv4() {
-    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, function (c) { return (parseInt(c) ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> parseInt(c) / 4).toString(16); });
+
+
+function generateUUIDv4(): string {
+    return "10000000-1000-4000-8000-100000000000".replace(
+        /[018]/g, (c) => (parseInt(c) ^ crypto.getRandomValues(
+            new Uint8Array(1))[0] & 15 >> parseInt(c) / 4
+        ).toString(16)
+    );
 }
+
+
 // interfaces & constance's **************************************************/
-var MessageSignal;
-(function (MessageSignal) {
-    MessageSignal["USER_CONNECTED"] = "USER_CONNECTED";
-    MessageSignal["USER_DISCONNECTED"] = "USER_DISCONNECTED";
-    MessageSignal["NEW_ICE_CANDIDATE"] = "NEW_ICE_CANDIDATE";
-    MessageSignal["VIDEO_OFFER"] = "VIDEO_OFFER";
-    MessageSignal["VIDEO_ANSWER"] = "VIDEO_ANSWER";
-    MessageSignal["HANG_UP"] = "HANG_UP";
-})(MessageSignal || (MessageSignal = {}));
+
+
+enum MessageSignal {
+    USER_CONNECTED = "USER_CONNECTED",
+    USER_DISCONNECTED = "USER_DISCONNECTED",
+    NEW_ICE_CANDIDATE = "NEW_ICE_CANDIDATE",
+    VIDEO_OFFER = "VIDEO_OFFER",
+    VIDEO_ANSWER = "VIDEO_ANSWER",
+    HANG_UP = "HANG_UP",
+}
+
+interface Message {
+    signal: MessageSignal
+    content: any
+}
+
+interface User {
+    id: string
+    name: string
+}
+
+interface NewIceCandidate {
+    userId: string
+    candidate: RTCIceCandidate | null
+}
+
+interface VideoSDP {
+    userId: string
+    sdp: RTCSessionDescription | null
+}
+
+interface HangUp {
+    userId: string
+}
+
 var mediaConstraints = {
     audio: true, // We want an audio track
     video: true // ...and we want a video track
-};
-var CURRENT_USER_NAME = "It's you";
-var USER_UUID = generateUUIDv4();
-var ws = new WebSocket("ws://localhost:8000/ws/".concat(USER_UUID));
-var myPeerConnection = null;
-var targetUserId = null;
+}
+
+const CURRENT_USER_NAME = "It's you"
+const USER_UUID = generateUUIDv4();
+const ws = new WebSocket(`ws://localhost:8000/ws/${USER_UUID}`);
+
+var myPeerConnection: RTCPeerConnection | null = null;
+var targetUserId: string | null = null;
+
+
 // ws actions ****************************************************************/
-ws.onmessage = function (event) {
-    var message = JSON.parse(event.data);
+
+
+ws.onmessage = (event) => {
+    let message: Message = JSON.parse(event.data);
     switch (message.signal) {
+
         case MessageSignal.USER_CONNECTED:
             clearUserList();
-            for (var _i = 0, _a = message.content; _i < _a.length; _i++) {
-                var user = _a[_i];
+            for (let user of message.content) {
                 addNewUser(user);
             }
             break;
+
         case MessageSignal.USER_DISCONNECTED:
             removeUser(message.content.id);
             break;
+
         case MessageSignal.VIDEO_OFFER:
             handleVideoOfferMsg(message.content);
             break;
+        
         case MessageSignal.VIDEO_ANSWER:
             console.log("Got VIDEO_ANSWER");
             break;
+
         case MessageSignal.NEW_ICE_CANDIDATE:
             handleNewICECandidateMsg(message.content);
             break;
+
         case MessageSignal.HANG_UP:
             closeVideoCall();
             break;
+
         default:
-            console.error("IDK how to read this data ".concat(message));
+            console.error(`IDK how to read this data ${message}`);
             break;
     }
 };
+
+
 // DOM actions ***************************************************************/
+
+
 function getUserListElement() {
-    var userList = document.getElementById('users');
+    let userList = document.getElementById('users');
     if (userList)
         return userList;
     throw new Error("Cannot get users list");
 }
-function getUserElement(userId) {
-    var userElement = document.getElementById(userId);
+
+function getUserElement(userId: string) {
+    let userElement = document.getElementById(userId);
     if (userElement)
         return userElement;
     throw new Error("Cannot get user");
 }
+
 function getHangupButtonElement() {
-    var hangupButtonElement = document.getElementById("hangup-button");
+    let hangupButtonElement = document.getElementById("hangup-button") as HTMLButtonElement;
     if (hangupButtonElement)
         return hangupButtonElement;
     throw new Error("Cannot get hangup button");
 }
+
 function getLocalVideoMediaElement() {
-    var localVideoElement = document.getElementById("local_video");
+    let localVideoElement = document.getElementById("local_video") as HTMLMediaElement;
     if (localVideoElement)
         return localVideoElement;
     throw new Error("Cannot get local video");
 }
+
 function getReceivedVideoMediaElement() {
-    var receivedVideoElement = document.getElementById("received_video");
+    let receivedVideoElement = document.getElementById("received_video") as HTMLMediaElement;
     if (receivedVideoElement)
         return receivedVideoElement;
     throw new Error("Cannot get received video");
 }
+
+
 // Web-RTC actions ***********************************************************/
+
+
 function closeVideoCall() {
-    var remoteVideo = getReceivedVideoMediaElement();
-    var localVideo = getLocalVideoMediaElement();
+    let remoteVideo = getReceivedVideoMediaElement();
+    let localVideo = getLocalVideoMediaElement();
+
     if (myPeerConnection) {
         myPeerConnection.ontrack = null;
         myPeerConnection.onicecandidate = null;
@@ -94,28 +153,36 @@ function closeVideoCall() {
         myPeerConnection.onsignalingstatechange = null;
         myPeerConnection.onicegatheringstatechange = null;
         myPeerConnection.onnegotiationneeded = null;
+
         if (remoteVideo.srcObject) {
-            remoteVideo.srcObject.getTracks().forEach(function (track) { return track.stop(); });
+            (<MediaStream>remoteVideo.srcObject).getTracks().forEach((track) => track.stop());
         }
+
         if (localVideo.srcObject) {
-            localVideo.srcObject.getTracks().forEach(function (track) { return track.stop(); });
+            (<MediaStream>localVideo.srcObject).getTracks().forEach((track) => track.stop());
         }
+
         myPeerConnection.close();
         myPeerConnection = null;
     }
+
     remoteVideo.removeAttribute("src");
     remoteVideo.removeAttribute("srcObject");
     localVideo.removeAttribute("src");
     remoteVideo.removeAttribute("srcObject");
+
     getHangupButtonElement().disabled = true;
     targetUserId = null;
-}
-;
-function handleGetUserMediaError(e) {
+};
+
+
+function handleGetUserMediaError(e: Error) {
     switch (e.name) {
         case "NotFoundError":
-            alert("Unable to open your call because no camera and/or microphone" +
-                "were found.");
+            alert(
+                "Unable to open your call because no camera and/or microphone" +
+                "were found.",
+            );
             break;
         case "SecurityError":
         case "PermissionDeniedError":
@@ -125,70 +192,75 @@ function handleGetUserMediaError(e) {
             alert("Error opening your camera and/or microphone: " + e.message);
             break;
     }
+
     closeVideoCall();
-}
-;
-function handleICECandidateEvent(event) {
+};
+
+function handleICECandidateEvent(event: RTCPeerConnectionIceEvent) {
     if (targetUserId) {
-        var content = {
+        
+        let content: NewIceCandidate = {
             userId: targetUserId,
             candidate: event.candidate,
-        };
-        var msg = {
+        }
+        let msg: Message = {
             signal: MessageSignal.NEW_ICE_CANDIDATE,
             content: content,
-        };
+        }
         sendMessage(msg);
-    }
-    else {
+
+    } else {
         console.warn("Event candidate is null!");
     }
-}
-;
-function handleTrackEvent(event) {
-    event.streams[0].onremovetrack = function (event) {
-        var stream = getReceivedVideoMediaElement().srcObject;
-        var trackList = stream.getTracks();
+};
+
+
+function handleTrackEvent(event: RTCTrackEvent) {
+    event.streams[0].onremovetrack = (event: MediaStreamTrackEvent) => {
+        let stream = (<MediaStream>getReceivedVideoMediaElement().srcObject);
+        let trackList = stream.getTracks();
         if (trackList.length == 0) {
             closeVideoCall();
         }
-    };
+    }
     getReceivedVideoMediaElement().srcObject = event.streams[0];
     getHangupButtonElement().disabled = false;
-}
-;
+
+};
+
+
 function handleNegotiationNeededEvent() {
     if (myPeerConnection) {
+
         myPeerConnection.createOffer()
-            .then(function (offer) {
-            if (myPeerConnection)
-                return myPeerConnection.setLocalDescription(offer);
-            throw new Error("Peer connection is't created!");
-        })
-            .then(function () {
-            if (myPeerConnection && targetUserId) {
-                var content = {
-                    userId: targetUserId,
-                    sdp: myPeerConnection.localDescription
-                };
-                var msg = {
-                    signal: MessageSignal.VIDEO_OFFER,
-                    content: content
-                };
-                sendMessage(msg);
-            }
-            else {
-                throw new Error("Peer connection or target user Id is null!");
-            }
-        })
+            .then((offer) => {
+                if (myPeerConnection)
+                    return myPeerConnection.setLocalDescription(offer);
+                throw new Error("Peer connection is't created!");
+            })
+            .then(() => {
+                if (myPeerConnection && targetUserId) {
+                    let content: VideoSDP = {
+                        userId: targetUserId,
+                        sdp: myPeerConnection.localDescription
+                    }
+                    let msg: Message = {
+                        signal: MessageSignal.VIDEO_OFFER,
+                        content: content
+                    }
+                    sendMessage(msg);
+                } else {
+                    throw new Error("Peer connection or target user Id is null!");
+                }
+            })
             .catch(reportError);
-    }
-    else {
+
+    } else {
         throw new Error("Peer connection or target user Id is null!");
     }
-}
-;
-function handleICEConnectionStateChangeEvent(event) {
+};
+
+function handleICEConnectionStateChangeEvent(event: Event) {
     if (myPeerConnection) {
         switch (myPeerConnection.iceConnectionState) {
             case "closed":
@@ -197,12 +269,11 @@ function handleICEConnectionStateChangeEvent(event) {
                 closeVideoCall();
                 break;
         }
-    }
-    else {
+    } else {
         throw new Error("Peer connection is null!");
     }
-}
-;
+};
+
 function handleSignalingStateChangeEvent(event) {
     if (myPeerConnection) {
         switch (myPeerConnection.signalingState) {
@@ -210,12 +281,11 @@ function handleSignalingStateChangeEvent(event) {
                 closeVideoCall();
                 break;
         }
-    }
-    else {
+    } else {
         throw new Error("Peer connection is null!");
     }
-}
-;
+};
+
 function createPeerConnection() {
     myPeerConnection = new RTCPeerConnection({
         iceServers: [
@@ -225,153 +295,161 @@ function createPeerConnection() {
             },
         ],
     });
+
     myPeerConnection.onicecandidate = handleICECandidateEvent;
     myPeerConnection.ontrack = handleTrackEvent;
     myPeerConnection.onnegotiationneeded = handleNegotiationNeededEvent;
     myPeerConnection.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
     myPeerConnection.onsignalingstatechange = handleSignalingStateChangeEvent;
-}
-;
+};
+
+
 /** business logic ***********************************************************/
+
+
 function inviteUser(event) {
     if (myPeerConnection) {
         alert("You can't start a call because you already have one open!");
-    }
-    else {
-        var clickedUserId = event.target.id;
+    } else {
+
+        let clickedUserId: string = event.target.id;
         if (clickedUserId === USER_UUID) {
             alert("I'm afraid I can't let you talk to yourself.");
             return;
         }
+
         targetUserId = clickedUserId;
         createPeerConnection();
         navigator.mediaDevices
             .getUserMedia(mediaConstraints)
-            .then(function (localStream) {
-            getLocalVideoMediaElement().srcObject = localStream;
-            localStream
-                .getTracks()
-                .forEach(function (track) {
-                if (myPeerConnection) {
-                    myPeerConnection.addTrack(track, localStream);
-                }
-                else {
-                    throw new Error("Peer connections is't created!");
-                }
-            });
-        })
+            .then((localStream) => {
+                getLocalVideoMediaElement().srcObject = localStream;
+                localStream
+                    .getTracks()
+                    .forEach((track) => {
+                        if (myPeerConnection) {
+                            myPeerConnection.addTrack(track, localStream)
+                        } else {
+                            throw new Error("Peer connections is't created!");
+                        }
+                    });
+            })
             .catch(handleGetUserMediaError);
     }
-}
-;
-function handleNewICECandidateMsg(newIceCandidate) {
+};
+
+function handleNewICECandidateMsg(newIceCandidate: NewIceCandidate) {
     var candidate = new RTCIceCandidate(newIceCandidate.candidate);
     if (myPeerConnection) {
         myPeerConnection.addIceCandidate(candidate).catch(reportError);
-    }
-    else {
+    } else {
         throw new Error("Peer connection is null!");
     }
 }
-function handleVideoOfferMsg(videOffer) {
-    var localStream = null;
+
+function handleVideoOfferMsg(videOffer: VideoSDP) {
+    let localStream: MediaStream | null = null;
+
     targetUserId = videOffer.userId;
     createPeerConnection();
+
     if (videOffer.sdp) {
         var desc = new RTCSessionDescription(videOffer.sdp);
-    }
-    else {
+    } else {
         throw new Error("SDP is null!");
     }
+
     if (myPeerConnection) {
         myPeerConnection
             .setRemoteDescription(desc)
             .then(function () {
-            return navigator.mediaDevices.getUserMedia(mediaConstraints);
-        })
+                return navigator.mediaDevices.getUserMedia(mediaConstraints);
+            })
             .then(function (stream) {
-            localStream = stream;
-            getLocalVideoMediaElement().srcObject = localStream;
-            localStream
-                .getTracks()
-                .forEach(function (track) {
-                if (myPeerConnection && localStream) {
-                    myPeerConnection.addTrack(track, localStream);
-                }
-                else {
-                    throw new Error("Peer connection or/and local stream is null!");
-                }
-            });
-        })
+                localStream = stream;
+                getLocalVideoMediaElement().srcObject = localStream;
+                localStream
+                    .getTracks()
+                    .forEach((track) => {
+                        if (myPeerConnection && localStream) {
+                            myPeerConnection.addTrack(track, localStream)
+                        } else {
+                            throw new Error("Peer connection or/and local stream is null!");
+                        }
+                    });
+            })
             .then(function () {
-            if (myPeerConnection)
-                return myPeerConnection.createAnswer();
-            throw new Error("Peer connection is null!");
-        })
+                if (myPeerConnection)
+                    return myPeerConnection.createAnswer();
+                throw new Error("Peer connection is null!");
+            })
             .then(function (answer) {
-            if (myPeerConnection)
-                return myPeerConnection.setLocalDescription(answer);
-            throw new Error("Peer connection is null!");
-        })
+                if (myPeerConnection)
+                    return myPeerConnection.setLocalDescription(answer);
+                throw new Error("Peer connection is null!");
+            })
             .then(function () {
-            if (targetUserId && myPeerConnection) {
-                var content = {
-                    userId: targetUserId,
-                    sdp: myPeerConnection.localDescription
-                };
-                var msg = {
-                    signal: MessageSignal.VIDEO_ANSWER,
-                    content: content
-                };
-                sendMessage(msg);
-            }
-            else {
-                throw new Error("Peer connection or target user id is null!");
-            }
-        })
+                if (targetUserId && myPeerConnection) {
+                    let content: VideoSDP = {
+                        userId: targetUserId,
+                        sdp: myPeerConnection.localDescription
+                    }
+                    let msg: Message = {
+                        signal: MessageSignal.VIDEO_ANSWER,
+                        content: content
+                    }
+                    sendMessage(msg);
+                } else {
+                    throw new Error("Peer connection or target user id is null!");
+                }
+            })
             .catch(handleGetUserMediaError);
-    }
-    else {
+    } else {
         throw new Error("Peer connection wasn't created");
     }
-}
-;
+};
+
 function hangUpCall() {
     closeVideoCall();
     if (targetUserId) {
-        var content = {
+        let content: HangUp = {
             userId: targetUserId
-        };
-        var msg = {
+        }
+        let msg: Message = {
             signal: MessageSignal.HANG_UP,
             content: content,
-        };
+        }
         sendMessage(msg);
     }
 }
-function sendMessage(msg) {
-    var msgJSON = JSON.stringify(msg);
+
+function sendMessage(msg: Message) {
+    let msgJSON: string = JSON.stringify(msg);
     ws.send(msgJSON);
 }
+
 function clearUserList() {
-    var userList = getUserListElement();
-    var users = userList.querySelectorAll('li');
-    users.forEach(function (li) {
+    let userList = getUserListElement();
+    const users = userList.querySelectorAll('li');
+    users.forEach(li => {
         li.remove();
     });
 }
-function addNewUser(user) {
-    var userList = getUserListElement();
-    var userEl = document.createElement('li');
-    var content = document.createTextNode(user.name);
+
+function addNewUser(user: User) {
+    let userList = getUserListElement();
+    let userEl = document.createElement('li');
+    let content = document.createTextNode(user.name)
     if (user.id === USER_UUID)
-        content = document.createTextNode(CURRENT_USER_NAME);
-    userEl.setAttribute("id", user.id);
+        content = document.createTextNode(CURRENT_USER_NAME)
+
+    userEl.setAttribute("id", user.id)
     userEl.addEventListener("click", inviteUser, false);
-    userEl.appendChild(content);
-    userList.appendChild(userEl);
+    userEl.appendChild(content)
+    userList.appendChild(userEl)
 }
-function removeUser(userId) {
-    var userElement = getUserElement(userId);
+
+function removeUser(userId: string) {
+    let userElement = getUserElement(userId);
     userElement.remove();
 }
